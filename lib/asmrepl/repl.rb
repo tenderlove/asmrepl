cc32b8a7045b2874c2e1d8590c5736837a2ff857
Aaron Patterson <tenderlove@ruby-lang.org>
2021-11-29 15:18:03 -0800

    Remove crabstone runtime dependency
    We don't actually use this right now, so I'll remove it
    
    Fixes #2

--- lib/asmrepl/repl.rb
+++ lib/asmrepl/repl.rb
@@ -1,23 +1,11 @@
 require "fiddle"
 require "fisk/helpers"
-require "crabstone"
 require "reline"
 
 if RUBY_PLATFORM =~ /darwin/
   require "asmrepl/macos"
 else
   require "asmrepl/linux"
-end
-
-class Crabstone::Binding::Instruction
-  class << self
-    alias :old_release :release
-  end
-
-  # Squelch error in crabstone
-  def self.release obj
-    nil
-  end
 end
 
 module ASMREPL
